b62129371b046a6ef3d6245434c0d2cd4b433305
Valério <valcorr29@gmail.com>
2024-02-08 16:17:38 -0300

    feito

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Integer numero;
+        String agencia;
+        String cliente;
+        Double saldo;
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.println("Por favor, digite o número da Agência !");
+            agencia = scanner.nextLine();
+            System.out.println("Por favor, digite o número da Conta !");
+            numero = scanner.nextInt();
+            scanner.nextLine(); // vai consumir a linha adicional já que nextInt não faz uma quebra de linha
+            System.out.println("Por favor, digite o Nome do Cliente !");
+            cliente = scanner.nextLine();
+            System.out.println("Por favor, digite o saldo !");
+            saldo = scanner.nextDouble();
+            //Saida
+            System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",cliente,agencia,numero,saldo);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+        
     }
 }
